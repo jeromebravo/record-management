3c0beb033ee647ac9b7acb2df16f06c317070ed1
Jerome Bravo <jeromebravo0608@gmail.com>
2021-03-05 13:07:08 +0800

    add border-bottom

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -2,15 +2,20 @@ import React from 'react';
 import {Container, Navbar, Nav, Button} from 'react-bootstrap';
 import {connect} from 'react-redux';
 import {logout} from '../../actions/auth';
+import {useLocation} from 'react-router-dom';
 
 const NavBar = ({logout}) => {
+    const location = useLocation();
+
+    console.log(location);
+
     return (
         <Navbar bg='primary' variant='dark' expand='lg'>
             <Container>
                 <Navbar.Brand href='/dashboard'>SCHOOL UTILITIES</Navbar.Brand>
                 <Nav>
-                    <Button href='/items' className='mr-5'>Items</Button>
-                    <Button href='/records' className='mr-5'>Records</Button>
+                    <Button href='/items' className={location.pathname === '/items' ? 'mr-5 b-bottom' : 'mr-5'}>Items</Button>
+                    <Button href='/records' className={location.pathname === '/records' ? 'mr-5 b-bottom' : 'mr-5'}>Records</Button>
                     <Button variant='danger' onClick={logout}>Logout</Button>
                 </Nav>
             </Container>
